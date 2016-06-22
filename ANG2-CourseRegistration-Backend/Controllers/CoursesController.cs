e34eef87b35d21bdc5071625aebd2ab4a4ccc035
Johan Herscheid <Johan.Herscheid@ictu.nl>
2016-06-22 13:37:40 +0200

    Changed CourseDto

--- ANG2-CourseRegistration-Backend/Controllers/CoursesController.cs
+++ ANG2-CourseRegistration-Backend/Controllers/CoursesController.cs
@@ -10,28 +10,53 @@ using System.Web.Http.Cors;
 namespace ANG2_CourseRegistration_Backend.Controllers
 {
     [DataContract]
-    public class CourseDto {
+    public class CourseDto
+    {
 
-        [DataMember(Name="id")]
+        [DataMember(Name = "id", IsRequired = false)]
+
         public int Id { get; set; }
 
-        [DataMember(Name="name")]
+        [DataMember(Name = "name")]
         public string Name { get; set; }
     }
 
     [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class CoursesController : ApiController
     {
+        private static List<CourseDto> Courses = new List<CourseDto>
+        {
+            new CourseDto { Id= 1, Name= "Typescript Course"},
+            new CourseDto { Id= 2, Name= "CSharp Course"},
+            new CourseDto { Id= 3, Name= "Angular 1.x Course"},
+            new CourseDto { Id= 4, Name= "Angular 2 Course"},
+            new CourseDto { Id= 5, Name= "Java Course"},
+            new CourseDto { Id= 6, Name= "Office 2015 Course"},
+            new CourseDto { Id= 7, Name= "Fixing Cars Course" },
+        };
+
         [HttpGet]
         public IHttpActionResult GetAll()
         {
-            var courses = new List<CourseDto>()
-            {
-                new CourseDto { Id = 1, Name = "JohansTest" },
-                new CourseDto { Id = 2, Name = "PaulsTest" },
-            };
+            return Ok(Courses);
+        }
 
-            return Ok(courses);
+        [HttpPost]
+        public IHttpActionResult Post(CourseDto course)
+        {
+            var courseToAdd = new CourseDto() { Id = Courses.Max(c => c.Id) + 1, Name = course.Name };
+            Courses.Add(courseToAdd);
+
+            return Ok(courseToAdd);
+        }
+
+        [HttpPut]
+        public IHttpActionResult Put([FromUri]int id, CourseDto course)
+        {
+            var courseToChange = Courses.Single(c => c.Id == id);
+            courseToChange.Name = course.Name;
+
+            return Ok(courseToChange);
         }
     }
 }
